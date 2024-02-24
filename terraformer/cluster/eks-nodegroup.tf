be15cb7c9dc1f91a229bcde02b8dfe7d68c45569
JeongJaeSoon <94jaesoon.jeong@gmail.com>
2024-02-25 02:11:16 +0900

    fix: Adjust scaling_config

--- terraformer/cluster/eks-nodegroup.tf
+++ terraformer/cluster/eks-nodegroup.tf
@@ -11,9 +11,9 @@ resource "aws_eks_node_group" "eks-nodegroup" {
   }
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     min_size     = 1
-    max_size     = 3
+    max_size     = 1
   }
 
   depends_on = [
